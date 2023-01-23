a3439cd53f18dc722916de3e0722e048a0d3fc32
Atsushi Hamada <a.hamada.biz@gmail.com>
2023-01-23 23:09:46 +0900

    Minor update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,10 @@ const routes: Routes = [];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes),
+    // RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, {
+      useHash: true,
+    }),
     StaticModule,
     AuthModule,
     StudentModule,
